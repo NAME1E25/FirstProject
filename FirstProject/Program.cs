c65b82cb6843b346b314c1953a688c478eb7bd4e
Konstantin <konstantinkoychev1234@gmail.com>
2026-07-02 13:04:25 +0300

    Add Delete Menu

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -58,7 +58,10 @@
                     case "3":
                         break;
 
-                    
+                    case "4":
+                        break;
+
+                   
                 }
                 
             }
